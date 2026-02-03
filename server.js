e3d2ef87cd5a93c5046c430447fc6628c821310c
unssssss <malvinvenan@gmail.com>
2026-02-03 15:20:17 +0700

    complete

--- server.js
+++ server.js
@@ -9,14 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Debug middleware
-app.use((req, res, next) => {
-  console.log("=== Request ===");
-  console.log("URL:", req.url);
-  console.log("Body:", req.body);
-  next();
-});
-
 ///////////////////////////////////////// MONGODB CONNECTION //////////////////////////////////////////
 mongoose
   .connect(process.env.MONGO_URI)
@@ -27,12 +19,6 @@ mongoose
 const userRoutes = require("./routes/users");
 const categories = require("./routes/categories");
 const books = require("./routes/books");
-
-// Test route - ADD THIS
-app.post("/api/test", (req, res) => {
-  console.log("Test route - Body:", req.body);
-  res.json({ received: req.body });
-});
 
 app.use("/api/users", userRoutes);
 app.use("/api/categories", categories);
